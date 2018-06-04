378b6e28cf671f312b9ac27bf2d8c5602c70cdf7
Jaden <jcottorone21405@gmail.com>
2018-06-04 22:24:28 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,14 @@
+def about_me
+  puts "My name is Jaden"
+  puts "I am from Colorado"
+  puts "I am 15 years old"
+  puts "I want to go to school for computer science"
+end
+
+def about_emma
+  puts "My sister's name is Emma"
+  puts "She is 22 years old"
+  puts "She is having a baby in July"
+  puts "She studies criminal justice"
+end
+
